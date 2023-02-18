c13826c206910c1524e536b186cd61da151ff8a5
Amin <aminfarid1234@gmail.com>
2023-02-18 11:50:28 +0330

    add loading detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import ScrollToTop from "./components/Base/ScrollTop";
 import Detail from "./components/Detail";
 import Layout from "./components/Layout";
 import HomePage from "./pages/Home";
@@ -7,6 +8,7 @@ import HomePage from "./pages/Home";
 function App() {
   return (
     <div className="bg-zinc-900">
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { apiConfig } from "../../config/api.config";
@@ -9,14 +9,19 @@ interface DetailProps extends React.PropsWithChildren {}
 
 const Detail: React.FunctionComponent<DetailProps> = () => {
   const { category, id } = useParams();
+  const [active, setActive] = useState<boolean>();
   const getDetail = async () => {
     const res = await tmdbApi.detail(category, id);
     return res.data;
   };
 
-  const { data, status } = useQuery("detail", getDetail, {
+  const { data, status, isFetching } = useQuery("detail", getDetail, {
     refetchOnMount: true,
   });
+
+  useEffect(() => {
+    setActive(isFetching);
+  }, [isFetching]);
 
   if (status === "loading") {
     return <div>loading...</div>;
@@ -27,51 +32,59 @@ const Detail: React.FunctionComponent<DetailProps> = () => {
   }
 
   return (
-    <div
-      className="banner flex md:flex-row flex-col gap-10 md:justify-between justify-end items-center text-white px-8 pb-4 md:px-20"
-      style={{
-        backgroundImage: `url(${apiConfig.originalImage(data.backdrop_path)})`,
-      }}
-    >
-      <img
-        className="md:w-64 w-32 z-10 rounded-3xl"
-        src={apiConfig.originalImage(data.poster_path)}
-      />
-      <div className="z-10 flex flex-col gap-2">
-        <h1 className="text-3xl font-bold">
-          {data.name || data.title}{" "}
-          <span className="font-normal text-gray-400 text-2xl">
-            {new Date(data.last_air_date || data.release_date).getFullYear()}
-          </span>
-        </h1>
+    <>
+      {!active && (
+        <div
+          className="banner flex md:flex-row flex-col gap-10 md:justify-between justify-end items-center text-white px-8 pb-4 md:px-20"
+          style={{
+            backgroundImage: `url(${apiConfig.originalImage(
+              data.backdrop_path
+            )})`,
+          }}
+        >
+          <img
+            className="md:w-64 w-32 z-10 rounded-3xl"
+            src={apiConfig.originalImage(data.poster_path)}
+          />
+          <div className="z-10 flex flex-col gap-2">
+            <h1 className="text-3xl font-bold">
+              {data.name || data.title}{" "}
+              <span className="font-normal text-gray-400 text-2xl">
+                {new Date(
+                  data.last_air_date || data.release_date
+                ).getFullYear()}
+              </span>
+            </h1>
 
-        <div className="flex gap-2">
-          {data.genres?.map((data: any) => (
-            <h3 key={data.id}>{data.name}</h3>
-          ))}
-
-          <h3>{category === "tv" ? "" : "." + data.runtime + "m"} </h3>
-        </div>
-        <p className="text-gray-400">{data.tagline}</p>
-
-        <h3 className="font-semibold text-lg">Overview</h3>
-
-        <p className="-96">{data.overview}</p>
-        {category === "tv" && (
-          <>
-            <h3 className="text-lg font-semibold">Creator</h3>
-
-            <div className="flex gap-3">
-              {data.created_by?.map((data: any) => (
-                <div className="flex flex-col" key={data.id}>
-                  <h1>{data.name}</h1>
-                </div>
+            <div className="flex gap-2">
+              {data.genres?.map((data: any) => (
+                <h3 key={data.id}>{data.name}</h3>
               ))}
+
+              <h3>{category === "tv" ? "" : "." + data.runtime + "m"} </h3>
             </div>
-          </>
-        )}
-      </div>
-    </div>
+            <p className="text-gray-400">{data.tagline}</p>
+
+            <h3 className="font-semibold text-lg">Overview</h3>
+
+            <p className="-96">{data.overview}</p>
+            {category === "tv" && (
+              <>
+                <h3 className="text-lg font-semibold">Creator</h3>
+
+                <div className="flex gap-3">
+                  {data.created_by?.map((data: any) => (
+                    <div className="flex flex-col" key={data.id}>
+                      <h1>{data.name}</h1>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
