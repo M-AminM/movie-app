df315092eb506aba557eb15467539c03214212c5
Amin <aminfarid1234@gmail.com>
2023-03-07 18:29:54 +0330

    header update

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header: React.FunctionComponent = () => {
   const links = [
@@ -11,12 +11,19 @@ const Header: React.FunctionComponent = () => {
   return (
     <nav className="flex justify-between md:px-20 px-8 items-center text-white absolute top-6 w-full z-10">
       <h1 className="font-bold sm:text-2xl text-xl">tMovies</h1>
-      <ul className="flex sm:gap-4 gap-2">
+      <ul className="flex justify-center items-center sm:bg-transparent sm:w-auto sm:pb-0 sm:relative sm:gap-4 fixed bottom-0 left-0 w-full bg-gray-600 pb-3 gap-2">
         {links.map((link) => (
           <li key={link.id}>
-            <Link className="font-semibold sm:text-base text-sm" to={link.url}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "font-semibold sm:text-base text-sm  border-b-2 border-red-600"
+                  : "font-semibold sm:text-base text-sm text-white "
+              }
+              to={link.url}
+            >
               {link.name}
-            </Link>
+            </NavLink>
           </li>
         ))}
       </ul>
